b7bff2c260101759e3c2dd15ec533a3be7e369fe
Wechpisit <80614665+Wechpisit@users.noreply.github.com>
2024-05-10 14:36:06 +0700

    Update color of tiles

--- Ep3.py
+++ Ep3.py
@@ -2,19 +2,27 @@
 #row = 3  # 3 pieces per row
 
 #Dictionary
-tilecolor = {'red' : 100, 'blue' :200, 'yellow' :300, 'black' :400}
+tilecolor = {'red' : 100, 'blue' :200, 'yellow' :300, 'black' :400, 'orange':500, 'green': 600}
+
+#Show color and price
+print('----สีและราคากระเบื้อง----')
+for c,p in tilecolor.items():
+        print(f'สี: {c} ราคา: {p}')
+
+
+
 
 #User input 
     #Try - Except
 try:
         tiles = int(input('คุณต้องการปูกระเบื้องทั้งหมดกี่แผ่น : '))
         row = int(input('ใน 1 แถว ต้องปูกระเบื้องกี่แผ่น : '))
-        color = input('ต้องการกระเบื้องสีอะไร [red, blue, yellow, black]: ')
+        color = input('ต้องการกระเบื้องสีอะไร [red, blue, yellow, black, orange, green]: ')
 except:
          print('กรอกข้อมูลเฉพาะตัวเลขเท่านั้น')
          tiles = int(input('คุณต้องการปูกระเบื้องทั้งหมดกี่แผ่น : '))
          row = int(input('ใน 1 แถว ต้องปูกระเบื้องกี่แผ่น : '))
-         color = input('ต้องการกระเบื้องสีอะไร [red, blue, yellow, black]: ')
+         color = input('ต้องการกระเบื้องสีอะไร [red, blue, yellow, black, orange, green]: ')
 
 #Calculation          
 total_row = tiles // row
@@ -29,3 +37,5 @@ print(f'ปูกระเบื้องได้ทั้งหมด {total_r
 print(f'เหลือกระเบื้องที่ไม่ได้ปูเต็มแถว {remain_tiles} แผ่น')
 print(f'ลูกค้าต้องซื้อกระเบื้องเพิ่ม {buy_more} แผ่น')
 print(f'รวมค่าใช้จ่ายสำหรับสั่งซื้อกระเบื้องเพิ่ม : {total_cost}')
+
+print('-----------End of code-----------\n\n')
